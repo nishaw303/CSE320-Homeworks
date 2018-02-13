ab83b1135140a9b8244f788d5468d885f457cace
Nicholas Shaw <nicholas.shaw@stonybrook.edu>
2018-02-13 02:51:15 -0800

    Fixed some rerrors with initial loading and reading of commands, and added more error handling

--- main.c
+++ main.c
@@ -1,57 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char** argv) {
-  /*
-  * Dummy values
-  */
-  int id = 1;
-  char* first_name = "Sergey";
-  char* last_name = "Madaminov";
-  float gpa = 3.41;
-  char* major = "CSE";
-  /*
-  * This formatting for the string
-  * that you are expected to follow
-  */
-  printf("%d %s %s %.2f %s\n", id, first_name, last_name, gpa, major);
+int stringLen(char *string){
+
+	int len = 0;
+	while(*string != '\0'){
+		len++;
+		string++;
+	}
+	return len;
+}
+
+int stringComp(char *string1, char *string2){
+	
+	while(*string1 == *string2){
+		if (*string1 == '\0' && *string2 == '\0'){
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int main(int argc, char **argv) {
+
+	extern char *optarg;
+	extern int optind;
+  	int op;
+  	int vflag = 0, iflag = 0, fflag = 0, mflag = 0, oflag = 0, error = 0;
+  	int id = 0;
+  	char *lastname, *major, *filename;
+  	FILE *mainfile;
+  	FILE *outputfile;
   
-  int op;
-  int vflag = 0, iglaf = 0, fflag = 0, mflag = 0, oflag = 0;
-  int id = 0;
-  char *id, *lastname, *major, *filename;
-  while ((op = getop(argc, argv, "v:ifmo")) != -1){
-  	switch(c){
-  		case "v":
-  			vflag = 1;
-  			break;
-  		case "i":
-  			iflag = 1;
-  			id = optarg;
-  			break;
-  		case "f":
-  			fflag = 1;
-  			lastname = optarg;
-  			break;
-  		case "m":
-  			mflag = 1;
-  			major = optarg;
-  			break;
-  		case "o":
-  			oflag = 1;
-  			filename = optarg;
-  			break;
+  	/* Taking in arguments from command line */
+  	while ((op = getopt(argc, argv, "vi:f:m:o:")) != -1){
+  		switch(op){
+  			case 'v':
+  				vflag = 1;
+  				break;
+  			case 'i':
+  				iflag = 1;
+  				id = atoi(optarg);
+  				break;
+  			case 'f':
+  				fflag = 1;
+  				lastname = optarg;
+  				break;
+  			case 'm':
+  				mflag = 1;
+  				major = optarg;
+  				break;
+  			case 'o':
+  				oflag = 1;
+  				filename = optarg;
+  				break;
+  			case '?':
+  				error = 1;
+  				break;
+  		
+  		}
   	}
-  }
-  
-  if ((optind + 1) > argc){
-  	printf("NO QUERY PROVIDED");
-  	return 1;
-  }
-  else if (vflag && (iflag || fflag || mflag)){
-  	printf("OTHER ERROR");
-  	return 1;
-  }
-  
-  return 0;
+  	
+  	/* Checking if there is at least on argument, opening source file and error checking */
+  	if (argc >= 3){
+  		mainfile = fopen(*(argv + 1), "r");
+  	}
+  	else {
+  		printf("NO QUERY PROVIDED");
+  		return 1;
+  	}
+  	if ((vflag && (iflag || fflag || mflag)) || error){
+  		printf("OTHER ERROR");
+  		return 1;
+  	}
+  	if (outputfile = fopen(filename, "r")){
+  		printf("FILE EXISTS");
+  		return 1;
+  	}
+
+  	return 0;
 }
