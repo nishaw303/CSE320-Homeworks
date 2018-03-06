1eb9f473156f1d8d0892179bb5e62f520ad0fab9
Nicholas Shaw <nicholas.shaw@stonybrook.edu>
2018-03-05 20:51:39 -0800

    Added ordering by ID and flag

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "helper.h"
 #include <inttypes.h>
 
-typedef struct header{
-	uint64_t size;
-	uint64_t ID;
-	uint64_t Flag;
-} Header;
+void swap(uint64_t* pointer, uint64_t* tmp_pointer, int size){
+	memmove(tmp_pointer, pointer, size);
+	int tmp_size = (int) *(pointer + size / 8) >> 3 << 3;
+	memmove(pointer, pointer + size / 8, tmp_size);
+	memmove(pointer + tmp_size / 8, tmp_pointer, size);
+}
 
 int main(int argc, char** argv) {
     if (*(argv + 1) == NULL) {
@@ -20,12 +21,98 @@ int main(int argc, char** argv) {
      * of the code.
      */
 
-	Header first_block_header;
-	uint64_t* first_block =  (uint64_t*) ram;
-	first_block_header.size = (*first_block >> 3) << 3;
-	first_block_header.ID = (*first_block & 6) >> 1;
-	first_block_header.Flag = *first_block & 1;
-
+	int i;
+	int buf_size = 128;
+	int total_size = 0;
+	uint64_t* pointer = (uint64_t*) ram;
+	uint64_t* tmp_pointer = (uint64_t*) tmp_buf;
+	cse320_sbrk(896);
+	
+	/* If pointer is at the beginning of the header:
+	 * size = *pointer >> 3 << 3
+	 * ID = *pointer & 6 >> 1
+	 * flag = *pointer & 1
+	 */
+	 
+	memmove(tmp_buf, ram, 1024); /* Move all blocks to temp memory */
+	
+	/* Now we can just iterate through each memory block, checking the ID, 
+	 * and over-writing main memory if we find one of the right ID
+	 */
+	
+	int ID;
+	int size = 0;
+	int flag = 0;
+	pointer = (uint64_t*) ram;
+	
+	for (ID = 1; ID <= 3; ID++){
+		tmp_pointer = (uint64_t*) tmp_buf;
+		
+		for (i = 0; i < 1024;){
+			size = (int) *tmp_pointer >> 3 << 3;
+			int curr_ID = (*tmp_pointer & 6) >> 1;
+			
+			if (size == 0)
+				break;
+			
+			if (curr_ID == ID){
+				memmove(pointer, tmp_pointer, size); /* Copy the block into main memory */
+				pointer += size / 8;
+			}
+			
+			i += size;
+			tmp_pointer += size / 8;
+		}
+	}
+	
+	/* All of the blocks should now be in order from lowest to highest ID, 
+	 * we can now swap them based on their allocation
+	 */
+	 
+	tmp_pointer = (uint64_t*) tmp_buf;
+	i = 0;
+	 
+	for (ID = 1; ID <= 3; ID++){
+		pointer = (uint64_t*) ram;
+		int curr_ID = (*pointer & 6) >> 1;
+		
+		while(curr_ID == ID){
+			size = (int) *pointer >> 3 << 3;
+			flag = *pointer & 1;
+			
+			// CHANGE THIS SO IT KEEPS MOVING IT BACK UNTIL IT'S AT THE 1 BLOCK
+			if (flag == 0){
+				int tmp_flag = (*(pointer + size / 8) & 6) >> 1;
+				int tmp_ID = (*(pointer + size / 8) & 6) >> 1;
+				if (tmp_flag == 1 && tmp_ID == curr_ID){ /* We have to swap them */
+					swap(pointer, tmp_pointer, size);
+				}
+				else
+					pointer += size / 8;
+			}
+			else{
+				pointer += size / 8;
+			}
+			
+			i += size;
+			curr_ID = (*pointer & 6) >> 1;
+		}
+	}
+	
+	/* All of the blocks should now be in order based on their ID, then their allocation flag, 
+	 * we can now swap them based on their size
+	 */
+	 
+	for (ID = 1; ID <= 3; ID++){
+		pointer = (uint64_t*) ram;
+		int curr_ID = (*pointer & 6) >> 1;
+		int flag = 1;
+		
+		for (flag = 1; flag >= 0; flag--){
+			
+		}
+	}
+	
     /*
      * Do not modify code below.
      */
